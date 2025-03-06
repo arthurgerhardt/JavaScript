102d2ebc8e176908e869690e2d76fa7239a32b68
arthurgerhardt <renewedjesus@gmail.com>
2025-03-06 12:07:45 -0300

    Curso de JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+console.log("Essa mensagem é do JS conectado com o HTML.");
+let nome = "Arthur";
+let sobrenome = "Gerhardt";
+console.log(nome + " " + sobrenome);
+// console.log(nome);

0fc177c66be4dd2041f27216554f7b4b6eba2ba5
arthurgerhardt <renewedjesus@gmail.com>
2025-03-12 11:34:57 -0300

    Aulas de JavaScript

--- script.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello, World!");
-console.log("Essa mensagem é do JS conectado com o HTML.");
-let nome = "Arthur";
-let sobrenome = "Gerhardt";
-console.log(nome + " " + sobrenome);
-// console.log(nome);
---
+++ scripts.js
@@ -0,0 +1,35 @@
+// Case Sensitive
+let username = "Arthur"
+let userName = "João"
+
+console.log(username)
+console.log(userName)
+
+// Podemos
+let $email = "rodrigo@email.com"
+let _email = "joao@gmail.com"
+let ação = "Cadastrar"
+let user_email = "ana@email.com"
+
+console.log($email)
+console.log(_email)
+console.log(ação)
+console.log(user_email)
+
+// Não podemos criar variáveis com palavras reservadas
+// let 1user = "Arthur"
+//let product name = "Teclado"
+
+// Recomendações camelCase
+let FullName = "Arthur Dias Gomes Gerhardt"
+// Criar nomes que fazem sentido
+let productName = "Notebook"
+let productPrice = 1200
+let firstName = "Arthur"
+let lastName = "Gerhardt"
+
+console.log(FullName)
+console.log(productName)
+console.log(productPrice)
+console.log(firstName)
+console.log(lastName)

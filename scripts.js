4af83a3f09287205b68e422ad208d323c5c95271
arthurgerhardt <renewedjesus@gmail.com>
2025-03-18 10:29:06 -0300

    Aulas de JavaScript

--- scripts.js
+++ scripts.js
@@ -1,99 +1,18 @@
-// // Tipos de dados
+// Estritamente igual a (tipo e valor)
+let one = 1;
+let oneString = '1';
+let two = 2;
+let twoString = '2';
 
-// // Template literals (Template strings, interpolação de strings)
-// let username = "Arthur"
-// let email = "arthur@email.com"
+console.log(one)
+console.log(two)
+console.log(one === oneString); // false
+console.log(one === 1); // true
+console.log(one === two); // false
+console.log(one === twoString); // false
 
-// // Concatenar strings
-// let message = "Olá, " + username + ", seu e-mail é " + email
-// console.log(message)
-
-// // Template literals
-// console.log(`Olá, ${username}. Você conectou seu e-mail  ${email}.`)
-// console.log("Login efetuado com sucesso.")
-// // ----------------------------------------------------------------------------------------
-
-// // Number
-
-// // Inteiro positivo.
-// console.log(typeof 5)
-
-// console.log(5)
-
-// // Inteiro negativo.
-// console.log(-5)
-
-// // Números reais ou float
-// console.log(127.50)
-
-// // Not a Number NaN
-// console.log(12.5 / "Arthur")
-
-// // ----------------------------------------------------------------------------------------
-// // Boolean
-
-// let isLoading = true
-// console.log(true)
-// console.log(false)
-// console.log(isLoading)
-
-// // ----------------------------------------------------------------------------------------
-// // Undefined e Null
-// let empytiness
-// console.log("O valor é: ", empytiness)
-
-// let empty = null
-// console.log("O valor é: ", empty)
-
-// // ----------------------------------------------------------------------------------------
-// // Conversão de tipos e coersão de tipos
-
-// let value = "9"
-// console.log("9")
-// console.log(typeof "9")
-// console.log(Number("9"))
-// console.log(typeof Number("9"))
-
-// let age = 18
-// console.log(typeof age)
-// console.log(String(age))
-// console.log(typeof String(age))
-
-// let option = 1
-// console.log(option)
-// console.log(typeof option)
-// console.log(Boolean(option))
-// console.log(typeof Boolean(option))
-
-// ----------------------------------------------------------------------------------------
-// Operadores aritméticos
-
-// Adição
-console.log("Soma: ", 12 + 8)
-console.log("Concatenação: ", "12" + "8")
-
-// Subtração
-console.log("Subtração: ", 12 - 8)
-
-// Multiplicação
-console.log("Multiplicação: ", 3 * 5.5)
-
-// Divisão
-console.log("Divisão: ", 12 / 2)
-
-// Resto da divisão
-console.log("Resto da divisão: ", 12 % 5)
-
-// Exponencial
-console.log("Exponencial: ", 2 ** 3)
-
-// ----------------------------------------------------------------------------------------
-// Incremento e decremento
-let number = 10
-console.log("Número: ", number)
-number++
-console.log("Incremento: ", number)
-console.log("Incremento: ", number++) // Incrementa depois
-console.log("Incremento: ", number)
-console.log("Incremento: ", ++number) // Incrementa antes
-console.log("Incremento: ", number)
+// Estritamente diferente de (tipo e valor)
+console.log(one !== oneString); // true
+console.log(one !== 1); // false
+console.log(one !== two); // true
+console.log(one !== twoString); // true

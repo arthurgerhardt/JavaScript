58f14fe008ab8a7c4b10b77e8cd80bc0a06f0684
arthurgerhardt <renewedjesus@gmail.com>
2025-03-21 11:32:04 -0300

    Aprendendo Try Catch e FInally

--- scripts.js
+++ scripts.js
@@ -1,33 +1,12 @@
-// If (Se) Else (Senão) Else If (Senão Se)
+let result = 1
 
-let hour = new Date().getHours() // Pega a hora atual do sistema
-let minutes = new Date().getMinutes() // Pega os minutos atuais do sistema
-console.log(hour + ":" + minutes) // Exibe a hora atual no console
-
-if (hour <= 12) {
-    console.log("Bom dia")
-    console.log(`São ${hour}:${minutes} `)
-}
-else if (hour > 12 && hour <= 18) {
-    console.log("Boa tarde")
-    console.log(`São ${hour}:${minutes} `)
+try {
+    if (result === 0) {
+        throw new Error('Result is zero')
     }
-else  {
-    console.log("Boa noite")
-    console.log(`São ${hour}:${minutes}`)
-}
-// Switch Case
-let option = 1
-switch (option) {
-    case 1:
-        console.log("Consultar pedido.")
-        break
-    case 2:
-        console.log("Falar com atendente.")
-        break
-    case 3:
-        console.log("Sair")
-        break
-    default:
-        console.log("Opção inválida")
+} catch (error) {
+    console.log('Error:', error.message)
+} finally {
+    console.log('Fim')
+    console.log('This will always run')
 }

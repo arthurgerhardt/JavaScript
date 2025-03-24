73e2cde73800f50a7428e1ee1feca3f82131e004
arthurgerhardt <renewedjesus@gmail.com>
2025-03-24 12:07:10 -0300

    Aulas de JavaScript, aprendendo funções.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,7 @@
-let result = 1
-
-try {
-    if (result === 0) {
-        throw new Error('Result is zero')
-    }
-} catch (error) {
-    console.log('Error:', error.message)
-} finally {
-    console.log('Fim')
-    console.log('This will always run')
+function sum (a, b) {
+    result = a + b;
+    console.log(result);
+  return result;
 }
+
+sum(8, 8);

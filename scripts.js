5536242c823124975187686c691ae915d6ba579b
arthurgerhardt <renewedjesus@gmail.com>
2025-03-27 10:48:04 -0300

    Aula de funções em JavaScript

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
-function sum (a, b) {
-    result = a + b;
-    console.log(result);
-  return result;
+// Callback function, é uma função passada para outra função como argumento.
+
+function execute(taskName, Callback) {
+    console.log(`Executando tarefa: ${taskName}`);
+
+    Callback();
 }
 
-sum(8, 8);
+function Callback() {
+    console.log('Tarefa completada.');
+}
+
+// Example of using the execute function with a callback
+execute('Download file', Callback);
+
+// Callback function with parameters
+execute('Upload file', function() {
+    console.log('Task completed with parameters!');
+}
+);
+
+// Utilizando uma arrow function como callback
+execute('Excluindo arquivo', () => {
+    console.log('Arquivo excluído com sucesso');
+});
+
+execute('Salvando arquivo', () => console.log('Arquivo salvo com sucesso'));

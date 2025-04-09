a9fd689397afa02787b7e6e7f50100366a754ff2
Arthur Gerhardt <renewedjesus@gmail.com>
2025-04-09 10:33:36 -0300

    Aprendendo a criar eventos com JavaScript.

--- template/scripts.js
+++ template/scripts.js
@@ -1,13 +1,23 @@
+const guests = document.querySelector("ul")
+
 const newGuest = document.createElement("li")
+newGuest.classList.add("guest")
+
 const guestName = document.createElement("span")
 
 guestName.textContent = "Diego"
 
-const newName = document.createElement("span")
-newName.textContent = "Fernandes"
+const guestSurname = document.createElement("span")
+guestSurnameName.textContent = "Fernandes"
 
-// Adiciona após o último filho
+// // Adiciona após o último filho
+// newGuest.append(guestName, guestSurname)
+
+// // Adiciona antes do primeiro filho
+// newGuest.prepend(guestSurnameName)
+
+// É mais simples o append e aceita apenas um argumento
 newGuest.appendChild(guestName)
-newGuest.appendChild(newName)
 
-console.log(newGuest)
+guests.append(newGuest)
+
